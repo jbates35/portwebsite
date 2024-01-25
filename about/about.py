f9ee21bdde5d0ee0aed7df418961c765ec1c0ea0
Jimmy Bates <jbates.productions@gmail.com>
2024-01-25 00:13:29 -0800

    added some layout stuff in about me

--- about/about.py
+++ about/about.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 import yaml
+import os
 
 about_bp = Blueprint(
     "about", __name__, template_folder="templates", static_folder="static"
@@ -8,7 +9,8 @@ about_bp = Blueprint(
 
 @about_bp.route("/")
 def about():
+    file_path = os.path.join(os.path.dirname(__file__), "yaml", "about.yaml")
     entries = None
-    with open("yaml/about.yaml", "r") as file:
+    with open(file_path, "r") as file:
         entries = yaml.safe_load(file)
-    return render_template("about.html", entries=entries)
+    return render_template("about.html", entries=enumerate(entries))
