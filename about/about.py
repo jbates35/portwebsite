a9226aa80f5c43d2c257fd8167b3914fc01348f9
Jimmy Bates <jbates.productions@gmail.com>
2024-01-24 18:02:48 -0800

    some changes

--- about/about.py
+++ about/about.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+import yaml
 
 about_bp = Blueprint(
     "about", __name__, template_folder="templates", static_folder="static"
@@ -8,5 +8,7 @@ about_bp = Blueprint(
 
 @about_bp.route("/")
 def about():
-    lines = ["Hello", "World", "How", "Are", "You"]
-    return render_template("about.html", lines=lines)
+    entries = None
+    with open("yaml/about.yaml", "r") as file:
+        entries = yaml.safe_load(file)
+    return render_template("about.html", entries=entries)
